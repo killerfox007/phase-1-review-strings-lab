354fa5c1b40cea7c5d8b5f03cfb1c2d743f2c758
Kaleb Fox <kalebfox7@yahoo.com>
2024-05-13 11:58:57 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !` 
+
+const upperCaseWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser} !`
+const excitedWelcomeMessage = upperCaseWelcomeMessage.toUpperCase();
+
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)} !`
